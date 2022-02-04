bc31df07d152c7fa41c94ea05d8c9bec63b8d14c
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2022-02-04 18:46:33 +0100

    ajout page 404 sobre

---
+++ vue/content/404.php
@@ -0,0 +1,11 @@
+<body>
+    <br />
+    <div class="container">
+        <h1>La page que vous demandez n'est pas attribuée</h1>
+        <h2>Page 404</h2>
+        <br />
+        <div class="row">
+        </div>
+    </div>
+
+    <body>
